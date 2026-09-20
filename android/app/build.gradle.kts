326959fb4dbd6041299ed948f7105fc51ece4ade
GhostPrimeX <elitecopa70@gmail.com>
2026-09-20 10:36:45 -0400

    brand: icono soldado + firma estable

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,11 +30,18 @@ android {
         versionName = flutter.versionName
     }
 
+        signingConfigs {
+        create("release") {
+            storeFile = file("ghost.keystore")
+            storePassword = "ghost12345"
+            keyAlias = "ghost"
+            keyPassword = "ghost12345"
+        }
+    }
+
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
